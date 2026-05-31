b0356a3b4b8b7b84bd4d24a2e78ed5ab2d90cd34
Christopher Villamayor <christophersvillamayor@gmail.com>
2026-05-31 17:13:43 -0500

    feat: init consts, gitignore, and OrdersAnalytics class

--- app/orders_analytics.py
+++ app/orders_analytics.py
@@ -1,22 +1,27 @@
 import pandas as pd
-"Complete thes functions or write your own to perform the following tasks"
 
-def calculate_profit_by_order(orders_df):
-    "Calculate profit for each order in the DataFrame"
+class OrdersAnalytics:
+    def __init__(self, csv_path):
+        self.csv_path = csv_path
+        self.orders_df = pd.read_csv(self.csv_path)
 
-    return orders_df
+    def calculate_profit_by_order(self):
+        "Calculate profit for each order in the DataFrame"
 
-def calculate_most_profitable_region(orders_df):
-    "Calculate the most profitable region and its profit"
-   
-    return
+        # return orders_df
+        return
 
-def find_most_common_ship_method(orders_df):
-    "Find the most common shipping method for each Category"
+    def calculate_most_profitable_region(self):
+        "Calculate the most profitable region and its profit"
     
-    return 
+        return
 
-def find_number_of_order_per_category( orders_df):
-    "find the number of orders for each Category and Sub Category"
+    def find_most_common_ship_method(self):
+        "Find the most common shipping method for each Category"
+        
+        return 
 
-    return
+    def find_number_of_order_per_category(self):
+        "find the number of orders for each Category and Sub Category"
+
+        return
--- terraform/assignment/vars.tfvars
+++ terraform/assignment/vars.tfvars
@@ -1,6 +1,6 @@
 # Update the following variables with your information
-candidate_name="Your Name Here"
-aws_profile="your-aws-profile"
+candidate_name="Christopher Villamayor"
+aws_profile="christophersvillamayor"
 
 # Please leave these values as they are
 env     = "dev"
