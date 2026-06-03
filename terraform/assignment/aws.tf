0eed298774014868150d767810c4508b10c9684a
Christopher Villamayor <christophersvillamayor@gmail.com>
2026-06-02 19:42:11 -0500

    fix: incorrect account id for bucket, dynamodb_table deprecated, missing key

--- terraform/assignment/aws.tf
+++ terraform/assignment/aws.tf
@@ -1,5 +1,6 @@
 # You will need to modify the key value in the backend block to a unique value for your assignment.
 
+# FIXME: probably should add required_providers
 provider "aws" {
   region = var.region_name
   profile = var.aws_profile
@@ -9,11 +10,10 @@ data "aws_caller_identity" "current" {}
 
 terraform {
   backend "s3" {
-    bucket         = "nmd-training-tf-states-706146613458"
-    ## update the key value to a unique value for your assignment
-    # key            = "assignment/update-the-name-here-nmd-assignment.tfstate"
+    bucket         = "nmd-training-tf-states-888577066340"
+    key            = "nmd-assignment-christophersvillamayor.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "nmd-training-tf-state-lock-table"    
+    use_lockfile   = true    
     encrypt        = true                   # Encrypts the state file at rest
   }
 }
