7a87ef3bbea67e13f66d1e0b3da8e13d9d544650
Christopher Villamayor <christophersvillamayor@gmail.com>
2026-06-04 17:57:21 -0500

    fix: lambda couldn't trigger properly

--- terraform/assignment/main.tf
+++ terraform/assignment/main.tf
@@ -45,7 +45,6 @@ resource "aws_s3_bucket_notification" "s3_notification" {
   lambda_function {
     lambda_function_arn = module.lambda_function.lambda_arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "*"
     filter_suffix       = ".csv"
   }
 }
