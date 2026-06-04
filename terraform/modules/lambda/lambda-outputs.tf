32095bff63c8707dba0d7e76825e85f3f8aba526
Christopher Villamayor <christophersvillamayor@gmail.com>
2026-06-04 18:18:56 -0500

    fix: access denied on read csv

--- terraform/modules/lambda/lambda-outputs.tf
+++ terraform/modules/lambda/lambda-outputs.tf
@@ -2,3 +2,8 @@
 output "lambda_arn" {
   value = aws_lambda_function.lambda_function.arn
 }
+
+output "lambda_role_name" {
+  description = "The name of the IAM role created for the Lambda function"
+  value       = aws_iam_role.lambda_exec_role.name 
+}
