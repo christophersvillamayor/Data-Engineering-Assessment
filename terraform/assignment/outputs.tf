622ac95746f4da214299add5d05b4c95433ed859
Christopher Villamayor <christophersvillamayor@gmail.com>
2026-06-05 21:56:48 -0500

    feat: add testing

--- terraform/assignment/outputs.tf
+++ terraform/assignment/outputs.tf
@@ -9,3 +9,7 @@ output "ecr_repository_name" {
 output "ecr_repository_url" {
     value = module.ecr_repo.ecr_repository_url
 }
+
+output "input_s3_name" {
+    value = aws_s3_bucket.input_s3.bucket
+}
