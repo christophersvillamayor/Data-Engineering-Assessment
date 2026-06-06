946043a8a2fefd0e2ee6e1c4c3e4c8aa2790d619
Christopher Villamayor <christophersvillamayor@gmail.com>
2026-06-05 23:24:32 -0500

    feat: add required providers

--- terraform/assignment/aws.tf
+++ terraform/assignment/aws.tf
@@ -1,6 +1,5 @@
 # You will need to modify the key value in the backend block to a unique value for your assignment.
 
-# FIXME: probably should add required_providers
 provider "aws" {
   region = var.region_name
   profile = var.aws_profile
@@ -9,6 +8,12 @@ data "aws_caller_identity" "current" {}
 
 
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.47.0"
+    }
+  }
   backend "s3" {
     bucket         = "nmd-training-tf-states-888577066340"
     key            = "nmd-assignment-christophersvillamayor.tfstate"
